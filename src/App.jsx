a19a7a96a5a21c2ffdcedf0f17e839593c2bf3b9
Rohit <rohit.rksaini28@gmail.com>
2026-02-26 17:01:41 +0530

    state task1

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,37 @@ import ClassBassed from "./AllTopics/01_TypeOfCompontes/ClassBassed"
 import HeaderComp from "./components/HeaderComp"
 import ThemeChange from "./AllTopics/02_States/ThemeChange"
 import Counter from "./AllTopics/02_States/Counter"
+import CounterP from "./Assimeant/Practics/CounterP"
+import ThemesP from "./Assimeant/Practics/ThemesP"
+import ShowHideText from "./Assimeant/Practics/ShowHideText"
+import StateTask1 from "./Assimeant/StateTask1"
 
 
 const App = () => {
     return (
         <>
+
+        {/* All Topic folder impot */}
+        
         {/* <HeaderComp/>
         <ClassBassed></ClassBassed>
         <StatesInFBC/> */}
-        <ThemeChange/>
-        <Counter/>
+        {/* <ThemeChange/>
+        <Counter/> */}
+
+
+
+        {/* Practics folder import */}
+
+        {/* <CounterP/>
+        <ThemesP/>
+        <ShowHideText/> */}
+
+
+
+        {/* Assiment folder import */}
+
+        <StateTask1/>
         </>
     )
 }
